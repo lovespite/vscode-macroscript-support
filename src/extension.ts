d1c709025f893b1133227fe328eb85463d1b327d
lovespite <i@lovespite.com>
2025-10-30 09:53:51 +0800

    ok

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,33 @@
 import * as vscode from 'vscode';
 
 // 关键字
-const keywords = ['if', 'else', 'endif', 'while', 'endwhile', 'break', 'label', 'goto', 'exit', 'script', 'endscript'];
+const keywords = ['if', 'else', 'endif', 'while', 'endwhile', 'break', 'label', 'goto', 'exit'];
 // 函数
-const functions = ['Delay', 'Mouse', 'Key', 'PixelColor', 'RGB', 'Custom'];
+const functions = ['Delay', 'MouseDown', 'MouseUp', 'MouseClick', 'KeyDown', 'KeyUp', 'KeyPress', 'PixelColor', 'RGB', 'Custom'];
 // 枚举值
-const mouseActions = ['LeftDown', 'LeftUp', 'RightDown', 'RightUp', 'MiddleDown', 'MiddleUp', 'Wheel', 'Move'];
-const keyActions = ['KeyDown', 'KeyUp'];
+const mouseButtons = ['Left', 'Right', 'Middle'];
+// const keyActions = ['KeyDown', 'KeyUp'];
 const commonKeys = [
-    'LControlKey', 'RControlKey', 'LShiftKey', 'RShiftKey', 'LMenu', 'RMenu',
-    'Enter', 'Space', 'Tab', 'Back', 'Delete', 'Escape',
-    "Home", "End", "PageUp", "PageDown",
-    "Left", "Up", "Right", "Down",
-    'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    'D0', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9',
-    "NumPad0", "NumPad1", "NumPad2", "NumPad3", "NumPad4", "NumPad5", "NumPad6", "NumPad7", "NumPad8", "NumPad9",
+    "None", "LButton", "RButton", "Cancel", "MButton", "XButton1", "XButton2", "Back", "Tab", "LineFeed",
+    "Clear", "Return", "Enter", "ShiftKey", "ControlKey", "Menu", "Pause", "CapsLock", "Capital", "HangulMode",
+    "HanguelMode", "KanaMode", "JunjaMode", "FinalMode", "HanjaMode", "KanjiMode", "Escape", "IMEConvert", "IMENonconvert", "IMEAccept",
+    "IMEAceept", "IMEModeChange", "Space", "Prior", "PageUp", "PageDown", "Next", "End", "Home", "Left",
+    "Up", "Right", "Down", "Select", "Print", "Execute", "Snapshot", "PrintScreen", "Insert", "Delete",
+    "Help", "D0", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8",
+    "D9", "A", "B", "C", "D", "E", "F", "G", "H", "I",
+    "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+    "T", "U", "V", "W", "X", "Y", "Z", "LWin", "RWin", "Apps",
+    "Sleep", "NumPad0", "NumPad1", "NumPad2", "NumPad3", "NumPad4", "NumPad5", "NumPad6", "NumPad7", "NumPad8",
+    "NumPad9", "Multiply", "Add", "Separator", "Subtract", "Decimal", "Divide", "F1", "F2", "F3",
+    "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "F13",
+    "F14", "F15", "F16", "F17", "F18", "F19", "F20", "F21", "F22", "F23",
+    "F24", "NumLock", "Scroll", "LShiftKey", "RShiftKey", "LControlKey", "RControlKey", "LMenu", "RMenu", "BrowserBack",
+    "BrowserForward", "BrowserRefresh", "BrowserStop", "BrowserSearch", "BrowserFavorites", "BrowserHome", "VolumeMute", "VolumeDown", "VolumeUp", "MediaNextTrack",
+    "MediaPreviousTrack", "MediaStop", "MediaPlayPause", "LaunchMail", "SelectMedia", "LaunchApplication1", "LaunchApplication2", "Oem1", "OemSemicolon", "Oemplus",
+    "Oemcomma", "OemMinus", "OemPeriod", "OemQuestion", "Oem2", "Oem3", "Oemtilde", "OemOpenBrackets", "Oem4", "Oem5",
+    "OemPipe", "OemCloseBrackets", "Oem6", "Oem7", "OemQuotes", "Oem8", "OemBackslash", "Oem102", "ProcessKey", "Packet",
+    "Attn", "Crsel", "Exsel", "EraseEof", "Play", "Zoom", "NoName", "Pa1", "OemClear", "KeyCode",
+    "Shift", "Control", "Alt", "Modifiers",
 ];
 
 export function activate(context: vscode.ExtensionContext) {
@@ -34,26 +46,19 @@ export function activate(context: vscode.ExtensionContext) {
                 new vscode.CompletionItem(kw, vscode.CompletionItemKind.Keyword)
             );
 
-            // 2. 枚举值补全
-            const mouseActionCompletions = mouseActions.map(val =>
+            // 2. 鼠标按钮补全
+            const mouseButtonCompletions = mouseButtons.map(val =>
                 new vscode.CompletionItem(val, vscode.CompletionItemKind.EnumMember)
             );
-            const keyActionCompletions = keyActions.map(val =>
-                new vscode.CompletionItem(val, vscode.CompletionItemKind.EnumMember)
-            );
+
+            // 3. 键值补全
             const commonKeyCompletions = commonKeys.map(val =>
                 new vscode.CompletionItem(val, vscode.CompletionItemKind.EnumMember)
             );
 
-            // 3. 函数和代码片段 (Snippets)
+            // 4. 函数和代码片段 (Snippets)
             const delaySnippet = new vscode.CompletionItem('Delay(milliseconds)', vscode.CompletionItemKind.Snippet);
             delaySnippet.insertText = new vscode.SnippetString('Delay(${1:1000})');
-
-            const mouseSnippet = new vscode.CompletionItem('Mouse(action, x, y, delay)', vscode.CompletionItemKind.Snippet);
-            mouseSnippet.insertText = new vscode.SnippetString('Mouse(${1|LeftDown,LeftUp,RightDown,RightUp,MiddleDown,MiddleUp,Move,Wheel|}, ${2:x}, ${3:y}, ${4:delay})');
-
-            const keySnippet = new vscode.CompletionItem('Key(action, key, delay)', vscode.CompletionItemKind.Snippet);
-            keySnippet.insertText = new vscode.SnippetString('Key(${1|KeyDown,KeyUp|}, ${2:LControlKey}, ${3:delay})');
 
             const pixelColorSnippet = new vscode.CompletionItem('PixelColor(x, y)', vscode.CompletionItemKind.Function);
             pixelColorSnippet.insertText = new vscode.SnippetString('PixelColor(${1:x}, ${2:y})');
@@ -85,20 +90,50 @@ export function activate(context: vscode.ExtensionContext) {
             const gotoSnippet = new vscode.CompletionItem('goto name', vscode.CompletionItemKind.Snippet);
             gotoSnippet.insertText = new vscode.SnippetString('goto ${1:MyLabel}');
 
-            const scriptSnippet = new vscode.CompletionItem('script(name) ... endscript', vscode.CompletionItemKind.Snippet);
-            scriptSnippet.insertText = new vscode.SnippetString(
-                'script(${1:MyScript})\n\nendscript'
+            const namedScriptSnippet = new vscode.CompletionItem('Script(content, name)', vscode.CompletionItemKind.Snippet);
+            namedScriptSnippet.insertText = new vscode.SnippetString(
+                'Script(${1:`\n\n`}, ${2:"MyScript"})'
             );
+
+            const unnamedScriptSnippet = new vscode.CompletionItem('Script(content)', vscode.CompletionItemKind.Snippet);
+            unnamedScriptSnippet.insertText = new vscode.SnippetString(
+                'Script(${1:`\n\n`})'
+            );
+
+            // 鼠标事件函数
+            const mouseMoveSnippet = new vscode.CompletionItem('MouseMove(dx, dy, delayMs)', vscode.CompletionItemKind.Function);
+            mouseMoveSnippet.insertText = new vscode.SnippetString('MouseMove(${1:dx}, ${2:dy}, ${3:delayMs})');
+
+            const mouseMoveToSnippet = new vscode.CompletionItem('MouseMoveTo(x, y, delayMs)', vscode.CompletionItemKind.Function);
+            mouseMoveToSnippet.insertText = new vscode.SnippetString('MouseMoveTo(${1:x}, ${2:y}, ${3:delayMs})');
+
+            const mouseDownSnippet = new vscode.CompletionItem('MouseDown(button, delayMs)', vscode.CompletionItemKind.Function);
+            mouseDownSnippet.insertText = new vscode.SnippetString('MouseDown(${1:Left}, ${2:delayMs})');
+
+            const mouseUpSnippet = new vscode.CompletionItem('MouseUp(button, delayMs)', vscode.CompletionItemKind.Function);
+            mouseUpSnippet.insertText = new vscode.SnippetString('MouseUp(${1:Left}, ${2:delayMs})');
+
+            const mouseClickSnippet = new vscode.CompletionItem('MouseClick(button, delayMs)', vscode.CompletionItemKind.Function);
+            mouseClickSnippet.insertText = new vscode.SnippetString('MouseClick(${1:Left}, ${2:delayMs})');
+
+            const mouseWheelSnippet = new vscode.CompletionItem('MouseWheel(amount, delayMs)', vscode.CompletionItemKind.Function);
+            mouseWheelSnippet.insertText = new vscode.SnippetString('MouseWheel(${1:amount}, ${2:delayMs})');
+
+            // 键盘事件函数
+            const keyDownSnippet = new vscode.CompletionItem('KeyDown(key1, key2, ..., delayMs)', vscode.CompletionItemKind.Function);
+            keyDownSnippet.insertText = new vscode.SnippetString('KeyDown(${1:Key}, ${2:delayMs})');
+            const keyUpSnippet = new vscode.CompletionItem('KeyUp(key1, key2, ..., delayMs)', vscode.CompletionItemKind.Function);
+            keyUpSnippet.insertText = new vscode.SnippetString('KeyUp(${1:Key}, ${2:delayMs})');
+            const keyPressSnippet = new vscode.CompletionItem('KeyPress(key1, key2, ..., delayMs)', vscode.CompletionItemKind.Function);
+            keyPressSnippet.insertText = new vscode.SnippetString('KeyPress(${1:Key}, ${2:delayMs})');
 
             // 聚合所有补全项
             return [
                 ...keywordCompletions,
-                ...mouseActionCompletions,
-                ...keyActionCompletions,
+                ...mouseButtonCompletions,
+                // ...keyActionCompletions,
                 ...commonKeyCompletions,
                 delaySnippet,
-                mouseSnippet,
-                keySnippet,
                 pixelColorSnippet,
                 rgbSnippet,
                 customSnippet,
@@ -107,7 +142,17 @@ export function activate(context: vscode.ExtensionContext) {
                 whileSnippet,
                 labelSnippet,
                 gotoSnippet,
-                scriptSnippet
+                namedScriptSnippet,
+                unnamedScriptSnippet,
+                mouseMoveSnippet,
+                mouseMoveToSnippet,
+                mouseDownSnippet,
+                mouseUpSnippet,
+                mouseClickSnippet,
+                mouseWheelSnippet,
+                keyDownSnippet,
+                keyUpSnippet,
+                keyPressSnippet,
             ];
         }
     });
